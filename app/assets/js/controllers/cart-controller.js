289c2d0aa40166c0e5e55790749056ea51646b4b
Caio Oliveira <contato@caiodesign.com>
2017-11-21 15:58:10 -0200

    some adjustes for a better code

--- app/assets/js/controllers/cart-controller.js
+++ app/assets/js/controllers/cart-controller.js
@@ -1,14 +1,15 @@
-angular.module('app').controller('CartController', function(cartStorage) {
+angular.module('app').controller('CartController', function (cartStorage, $rootScope) {
     var _this = this;
     _this.cartStorage = cartStorage.cart;
 
     _this.removeFromCart = function(item) {
         item.addedToCart = false;
-        item.showAddToCart = false;
+        $rootScope.cartQuantity = $rootScope.cartQuantity - 1;
         var itemIndex = _this.cartStorage.items.indexOf(item);
         
         if (itemIndex > -1) {
             _this.cartStorage.items.splice(itemIndex, 1);
         }
+
     }
 });
--- app/assets/js/controllers/product-controller.js
+++ app/assets/js/controllers/product-controller.js
@@ -21,28 +21,24 @@ angular.module('app').factory('cartStorage', function() {
             description: "32GB - 4G Anatel",
             image: "http://phonesstorekenya.com/wp-content/uploads/2016/05/iphone-6s-good9-128x128.jpg",
             price: 1600,
-            showAddToCart: false,
             addedToCart: false
         }, {
             name: 'Samsung Galaxy 10',
             description: "32GB - 4G Anatel",
             image: "http://havan.vteximg.com.br/arquivos/ids/2560413-128-128/celular-smartphone-dual-chip-samsung-galaxy-j105-3-168.jpg?v=635972670480730000",
             price: 2000,
-            showAddToCart: false,
             addedToCart: false
         }, {
             name: 'Motorola V4',
             description: "32GB - 4G Anatel",
             image: "http://tripplek.co.ke/wp-content/uploads/2017/01/Motorola-Moto-g4-plus-0.jpg-good-128x128.jpg",
             price: 400,
-            showAddToCart: false,
             addedToCart: false
         }, {
             name: 'Sony Xperia',
             description: "32GB - 4G Anatel",
             image: "https://renovecel.com.br/wp-content/uploads/2016/10/107096-1-smartphone_sony_xperia_z1_c6943_preto_box-5-128x128.jpg",
             price: 1200,
-            showAddToCart: false,
             addedToCart: false
         }];
 
