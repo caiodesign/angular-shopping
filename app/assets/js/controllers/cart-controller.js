3050770f783c2c37e00eed66cdc3e88839f6f958
Caio Oliveira <contato@caiodesign.com>
2017-11-20 23:22:58 -0200

    v.0.1

---
+++ app/assets/js/controllers/cart-controller.js
@@ -0,0 +1,13 @@
+angular.module('app').controller('CartController', function(cartStorage) {
+    var _this = this;
+    _this.cartStorage = cartStorage.cart;
+
+    _this.removeFromCart = function(item) {
+        item.addedToCart = false;
+        item.showAddToCart = false;
+        var itemIndex = _this.cartStorage.items.indexOf(item);
+        if (itemIndex > -1) {
+            _this.cartStorage.items.splice(itemIndex, 1);
+        }
+    }
+});
